a495b584ed3c61c2524b57ce8d7715f70b3edc30
Gleyson Bernardo <gleyson.bernardo@gmail.com>
2019-02-08 17:29:54 -0200

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/gleribeiro/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.gleribeiro.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gleribeiro.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
